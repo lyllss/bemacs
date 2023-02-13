202f5a6bedd140071d1237c17061c80636f47098
Li Yu <liyu@google.com>
2023-02-13 21:34:41 +0800

    set auto full pairs)

--- config/init-clojure.el
+++ config/init-clojure.el
@@ -3,6 +3,15 @@
 
 (require 'cider)
 
+(require 'smartparens-config)
+(add-hook 'clojure-mode-hook #'smartparens-mode)
+(add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(require 'aggressive-indent)
+(add-hook 'prog-mode-hook #'aggressive-indent-mode)
+
+
 ;;(require 'inf-clojure)
 ;;(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
 (provide 'init-clojure)
